abd8439fd632b2b44513f4183e9b79849f1e87f6
boesecke <ina.boesecke@uni-konstanz.de>
2018-11-15 13:27:56 +0200

    Add necessary windows files, extend main.py to automaticaly recognize the os

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import os, sys, inspect, thread, time
+import os, sys, inspect, thread, time, platform
+
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-# Windows and Linux
-arch_dir = 'LeapSDK/lib/x64' if sys.maxsize > 2**32 else 'LeapSDK/lib/x86'
-# Mac
-#arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+
+if(platform.system() == 'Linux'):
+    arch_dir = 'LeapSDK/lib/x64' if sys.maxsize > 2**32 else 'LeapSDK/lib/x86'
+elif(platform.system() == 'Windows'):
+    arch_dir = 'LeapSDKWin' if sys.maxsize > 2**32 else 'LeapSDKWin'
+elif(platform.system() == 'Darwin'):
+    arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap
-
 
 class SampleListener(Leap.Listener):
 
@@ -20,7 +23,7 @@ class SampleListener(Leap.Listener):
         frame = controller.frame()
         print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
           frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
-
+          
 def main():
     # Create a sample listener and controller
     listener = SampleListener()
