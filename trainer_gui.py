5fbf4e626b42963bd648d6b65c179f2a580279ff
enesozi <enesozipek05@gmail.com>
2018-11-15 11:02:59 +0200

    Submit functionality added

--- trainer_gui.py
+++ trainer_gui.py
@@ -1,26 +1,54 @@
 from Tkinter import *
-
+from Database.db_config import DB
+from functools import partial
 
 
 class TrainGUI:
 
-    def __init__(self):
+    TOTAL_TRAINED_DATA = 0
+    LABEL_TEXT = None
+    
+    
+    def __init__(self,*args):
         self.root = Tk()
-        self.label = Label(self.root, text="LABEL")
-        self.entry = Entry(self.root, bd = 5)
-        self.submit = Button(self.root, text ="Submit", command=self.label_submitted)
+        TrainGUI.LABEL_TEXT = StringVar()
+        TrainGUI.LABEL_TEXT.set("TOTAL TRAINED DATA: %d"%TrainGUI.TOTAL_TRAINED_DATA)
+        self.root.geometry("300x200+500+200")
+        self.entry = Entry(self.root, bd = 5)    
+        self.label = Label(self.root, textvariable=TrainGUI.LABEL_TEXT)
+        self.submit = Button(self.root, text ="Submit", command=partial(self.label_submitted, *args))
+        self.db = DB('leapd')
 
+    def label_submitted(self,*args):
+        label = self.entry.get()
+        if len(label) != 1 or not label.isalpha():
+            print 'Please enter a valid label'
+            return
+        TrainGUI.TOTAL_TRAINED_DATA += 1
+        TrainGUI.LABEL_TEXT.set("TOTAL TRAINED DATA: %d"%TrainGUI.TOTAL_TRAINED_DATA)
+        self.root.update_idletasks()
+        self.quit()
+        for count, thing in enumerate(args):
+            print( '{0}. {1}'.format(count, thing))
 
-    def label_submitted(self):
-        print self.entry.get()
+    def change_train_data(self,*args):
+        self.submit.config(command=partial(self.label_submitted, *args))
+        self.root.update_idletasks()
 
-    def run(self):
+    def run(self,*args):
         self.label.pack()
         self.entry.pack()
-        self.submit.pack(side =BOTTOM) 
+        self.submit.pack(side = BOTTOM) 
+        self.change_train_data(*args)
         self.root.mainloop()
+
+    def quit(self):
+        self.root.quit()
 
 
 # if __name__ == '__main__':
-#     train_gui = TrainGUI()
-#     train_gui.run()
+#     train_gui = TrainGUI('apple', 'banana', 'cabbage')
+#     train_gui.run('k','k','j')
+#     train_gui.quit()
+#     train_gui.run('ka','ff','asd')
+#     print train_gui.TOTAL_TRAINED_DATA
