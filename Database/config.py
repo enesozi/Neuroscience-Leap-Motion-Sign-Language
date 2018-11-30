2ccf241f89c0a03b805eb6221795d29b96e24781
enesozi <enesozipek05@gmail.com>
2018-11-30 19:59:05 +0200

    credentials set with env variables

--- Database/config.py
+++ Database/config.py
@@ -1,10 +1,12 @@
-UNAME = "utcns_admin"
-PASSWORD = "Rrmaa5ctTtMKtKf"
+import os
+
+UNAME = os.environ.get('CNS_UNAME')
+PASSWORD = os.environ.get('CNS_PASSWORD')
 DBNAME = "utcns"
 PORT = 25263
 COLLECTION = "train_data"
 # with label
 NUMBER_OF_ATTRIBUTES = 61
-NUM_SAMPLES = 100
+NUM_SAMPLES = 150
 SAMPLE_DELAY = 1
 NUM_FEATURES = 60
